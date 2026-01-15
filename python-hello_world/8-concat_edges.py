853515e4254598237aa3b2c7d5050f6e7dbed6f7
laradreamer79 <laradreamer79@gmail.com>
2026-01-15 11:28:50 +0000

    fix code

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[8:27] + str[67:73])
+str = str[39:67] + str[8:27] + str[67:73]
+print(str)
