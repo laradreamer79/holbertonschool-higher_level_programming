90c2f07f5aca5e9e55103ba1497a4879d6529658
laradreamer79 <laradreamer79@gmail.com>
2026-01-21 14:19:43 +0000

    function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,14 +3,11 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints My name is <first_name> <last_name>"""
+    """Prints My name is <first name> <last name>"""
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    if last_name == "":
-        print("My name is {}".format(first_name))
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
