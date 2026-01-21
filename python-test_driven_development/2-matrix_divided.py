83101f3170d45abac2fc6e8c66432af0a05da789
laradreamer79 <laradreamer79@gmail.com>
2026-01-21 09:26:47 +0000

    update the code

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
+"""Module that defines matrix_divided function"""
+
+
 def matrix_divided(matrix, div):
+    """Divides all elements of a matrix by div
+
+    Args:
+        matrix (list of lists): matrix of integers or floats
+        div (int or float): divisor
+
+    Returns:
+        list of lists: new matrix with divided values
+
+    Raises:
+        TypeError: if matrix is not a matrix of integers/floats
+        TypeError: if rows of matrix are not the same size
+        TypeError: if div is not a number
+        ZeroDivisionError: if div is 0
+    """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
-            not all(isinstance(num, (int, float)) for row in matrix for num in row)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            not all(isinstance(num, (int, float))
+                    for row in matrix for num in row)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
