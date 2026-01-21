cc4d60f07aae3a1fdfedb2fe9d32b4ec91f55e66
laradreamer79 <laradreamer79@gmail.com>
2026-01-21 14:51:37 +0000

    update code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,26 +3,17 @@
 
 
 def text_indentation(text):
-    """Prints text with 2 new lines after ., ? and :"""
-
+    """Prints a text with 2 new lines after '.', '?' and ':'."""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    result = ""
-    i = 0
+    line = ""
+    for ch in text:
+        line += ch
+        if ch in ".?:":
+            print(line.strip())
+            print()
+            line = ""
 
-    while i < len(text):
-        result += text[i]
-
-        if text[i] in ".?:":
-            result += "\n\n"
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
-            continue
-
-        i += 1
-
-    lines = result.split("\n")
-    for line in lines:
-        print(line.strip())
+    if line.strip() != "":
+        print(line.strip(), end="")
