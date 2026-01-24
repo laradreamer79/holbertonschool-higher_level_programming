e85274b7541e283d07bb15a3e54d74cd8a6b2f40
laradreamer79 <laradreamer79@gmail.com>
2026-01-24 18:51:34 +0300

    Rename function to print_and_count_integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""Print and count integers in a list."""
 
-def safe_print_list_integers(my_list=[], x=0):
+
+def print_and_count_integers(my_list=[], x=0):
+    """Print the first x elements of a list that are integers and return count."""
     count = 0
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-            except (ValueError, TypeError):
-                pass
-    except IndexError:
-        pass
+    for i in range(x):
+        # IMPORTANT: do NOT catch IndexError here (so traceback appears if x is too big)
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (TypeError, ValueError):
+            pass
     print()
     return count
