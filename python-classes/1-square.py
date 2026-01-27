b2699eadbbba3ea3f64dee20dd0745c5d6cc11e0
laradreamer79 <laradreamer79@gmail.com>
2026-01-27 11:02:01 +0000

    make the file excutabl

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+class Square:
+    """Represents a square with a private size."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
