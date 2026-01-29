76438929bd19d6eabc39ade215ccc9185f871f7b
laradreamer79 <laradreamer79@gmail.com>
2026-01-29 17:13:00 +0300

    A square is a rectangle

---
+++ python-more_classes/9-main.py
@@ -0,0 +1,4 @@
+!/usr/bin/python3
+Rectangle = import(‘9-rectangle’).Rectangle
+
+mysquare = Rectangle.square(5) print(“Area: {} - Perimeter: {}”.format(mysquare.area(), mysquare.perimeter())) print(mysquare)
---
+++ python-more_classes/9-rectanglle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle."""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Retrieve the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width with validation."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
