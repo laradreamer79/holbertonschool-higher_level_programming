1fe64356fbeb5c4964dc509528093a2a97d8e96b
laradreamer79 <laradreamer79@gmail.com>
2026-01-29 22:50:30 +0300

    Extending the Python List with Notifications

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Defines VerboseList, a subclass of list that prints notifications
+when items are added or removed.
+"""
+
+
+class VerboseList(list):
+    """
+    VerboseList extends the built-in list class and prints messages
+    whenever the list is modified.
+    """
+
+    def append(self, item):
+        """
+        Add an item to the list and print a notification.
+        """
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        """
+        Extend the list with elements from iterable and print a notification.
+        """
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
+
+    def remove(self, item):
+        """
+        Remove an item from the list and print a notification.
+        """
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        """
+        Pop an item from the list and print a notification.
+        """
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
