b3942baa8240f1370bc88537bff7fb481adc367c
laradreamer79 <laradreamer79@gmail.com>
2026-02-03 17:19:12 +0300

    Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,11 +4,13 @@
 class Student:
     """Represents a student."""
 
+
     def __init__(self, first_name, last_name, age):
         """Initializes a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
 
     def to_json(self):
         """Returns the dictionary description of the Student."""
