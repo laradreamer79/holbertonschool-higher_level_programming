6d41fa2e605fe53df313ffb4dffb7e7e6b8caccb
laradreamer79 <laradreamer79@gmail.com>
2026-02-03 17:07:52 +0300

    Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Returns the dictionary description for JSON serialization of an object."""
+
+def class_to_json(obj):
+    """Returns the dictionary description for JSON serialization of an object."""
+    return obj.__dict__
