728776d9043efc1cabd22ac24768f5d1e1c7ebcf
laradreamer79 <laradreamer79@gmail.com>
2026-02-03 17:29:41 +0300

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary description of the Student."""
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
