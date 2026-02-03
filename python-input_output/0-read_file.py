2a7a8a87176e45aec83beee5b1b8722f59fbc4f9
laradreamer79 <laradreamer79@gmail.com>
2026-02-03 12:06:37 +0300

    document the function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 """function that reads a text file (UTF8) and prints it to stdout"""
 
 def read_file(filename=""):
-        """Reads a text file (UTF8) and prints it to stdout."""
+    """Reads a text file (UTF8) and prints it to stdout."""
     with open (filename, encoding="utf-8") as f:
         print(f.read(), end="")
