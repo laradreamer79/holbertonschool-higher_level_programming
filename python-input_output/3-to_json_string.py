09f83a750dfdbd40f5d0f7d2dc89c3024038c7b5
laradreamer79 <laradreamer79@gmail.com>
2026-02-03 15:09:41 +0300

    JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that returens the JSON repesentation of an object"""
+import json
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object"""
+    return json.dumps(my_obj)
