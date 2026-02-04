1471e117b1bca6a7daa8e6a2484102dffb8970ba
laradreamer79 <laradreamer79@gmail.com>
2026-02-04 11:34:10 +0300

    fix spelling mistake

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""pickle serialization module for saving and loading python objects."""
+"""Pickle serialization module for saving and loading Python objects."""
 
 import pickle
 
 
-def serialize__and_save_to_file(data, filename):
-    """serialize a Python object and save it to a pickle file."""
-    with open(filename, mode"wb") as f:
+def serialize_and_save_to_file(data, filename):
+    """Serialize a Python object and save it to a pickle file."""
+    with open(filename, mode="wb") as f:
         pickle.dump(data, f)
 
 
-def load_and_deserilize(filename):
-    """ load a pickle file and return the deserialized Python object."""
+def load_and_deserialize(filename):
+    """Load a pickle file and return the deserialized Python object."""
     with open(filename, mode="rb") as f:
         return pickle.load(f)
