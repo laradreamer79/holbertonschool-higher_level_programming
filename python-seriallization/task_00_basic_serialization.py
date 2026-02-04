1e35c656a1496eba2c11c42b42492179997bbdf4
laradreamer79 <laradreamer79@gmail.com>
2026-02-04 11:12:06 +0300

    Basic Serialization

---
+++ python-seriallization/task_00_basic_serialization.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic serialization module for saving and loading JSON data."""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a dictionary and save it to a JSON file."""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """Load JSON data from a file and return it as a dictionary."""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(f)
