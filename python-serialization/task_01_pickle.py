b675d663c034791b194d0dcfe4224220725ea4ba
laradreamer79 <laradreamer79@gmail.com>
2026-02-04 11:29:35 +0300

    pickling custom classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""pickle serialization module for saving and loading python objects."""
+
+import pickle
+
+
+def serialization_and_save_to_file(data, filename):
+    """serialize a Python object and save it to a pickle file."""
+    with open(filename, mode"wb") as f:
+        pickle.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """ load a pickle file and return the deserialized Python object."""
+    with open(filename, mode="rb") as f:
+        return pickle.load(f)
