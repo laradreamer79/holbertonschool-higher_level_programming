ab271e52cb12af73d61ec9cebc30fb9f2537e0ae
laradreamer79 <laradreamer79@gmail.com>
2026-02-27 17:17:08 +0300

    Fix Task 2 using .format()

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     query = (
         "SELECT id, name FROM states "
-        "WHERE name = '{}' "
+        "WHERE BINARY name = '{}' "
         "ORDER BY id ASC"
     ).format(state_name)
 
