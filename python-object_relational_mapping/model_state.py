0d33b59ff86cea8830908fe485af4aa5538f138f
laradreamer79 <laradreamer79@gmail.com>
2026-02-27 17:31:26 +0300

    Add State model using SQLAlchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+model_state.py
+
+Contains the class definition of a State
+and Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class that links to the MySQL table states"""
+    __tablename__ = 'states'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
+    )
+
+    name = Column(
+        String(128),
+        nullable=False
+    )
