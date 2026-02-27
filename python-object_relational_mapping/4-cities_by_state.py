9591612e1654ad63a3a8ba9a17f066aaa7ec691f
laradreamer79 <laradreamer79@gmail.com>
2026-02-27 17:24:15 +0300

    Add Task 4 cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+4-cities_by_state.py
+
+Lists all cities of a given state.
+Safe from MySQL injections.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """,
+        (state_name,)
+    )
+
+    cities = cursor.fetchall()
+    print(", ".join(city[0] for city in cities))
+
+    cursor.close()
+    db.close()
