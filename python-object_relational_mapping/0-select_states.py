92e100cc1f4291585c240eb611548a3d3775838e
laradreamer79 <laradreamer79@gmail.com>
2026-02-27 13:43:27 +0300

    Streamline row fetching and printing
    Refactor fetching and printing of rows to streamline code.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -24,10 +24,9 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    rows = cursor.fetchall()
-
-    for row in rows:
+        for row in cursor.fetchall():
         print(row)
+
 
     cursor.close()
     db.close()
